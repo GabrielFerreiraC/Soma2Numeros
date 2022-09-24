7197f5cb1e40cb9c0a03d07c01c18e6c4419f811
Gabriel Ferreira <gfdc2204@gmail.com>
2022-09-23 21:58:55 -0300

    Adiciona a Solução do Exercício

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int primeiroNumero, segundoNumero, soma;
+
+Console.WriteLine ("Cálculo da soma entre dois números .\n");
+
+
+Console.WriteLine("Digite o primeiro número: ");
+primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+soma = primeiroNumero + segundoNumero;
+
+Console.WriteLine($"\nSoma: {soma}");
